f0599be09a1eed55face04528319ae70956c2802
hieple <hieplevuc@gmail.com>
2024-06-13 20:26:17 +0700

    feat: Update controller to verify webhook

--- controllers/webhook-controller.js
+++ controllers/webhook-controller.js
@@ -1,12 +1,18 @@
-const { findDuplicates, performActionOnDuplicates, getConfig, handleColumnValueChange } = require('../services/duplicate.service');
-
+const {
+  findDuplicates,
+  performActionOnDuplicates,
+  getConfig,
+  handleColumnValueChange,
+} = require("../services/duplicate.service");
 
 const handleWebhook = async (req, res) => {
-  const { event } = req.body;
-  if (event && event.type === 'change_column_value') {
-    await handleColumnValueChange(event);
-  }
-  res.sendStatus(200);
+  // const { event } = req.body;
+  // if (event && event.type === 'change_column_value') {
+  //   await handleColumnValueChange(event);
+  // }
+  // res.sendStatus(200);
+  console.log(JSON.stringify(req.body, 0, 2));
+  res.status(200).send(req.body);
 };
 
 module.exports = {
